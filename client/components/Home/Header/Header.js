3bf38f0ef3eb39dbba9a148c0841d926152f14f4
aliraza1231 <ali.raza1231@outlook.com>
2024-09-17 16:45:21 +0500

    Updated design for banner header and added some more images.

--- client/components/Home/Header/Header.js
+++ client/components/Home/Header/Header.js
@@ -4,6 +4,9 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import sliderImg1 from "../../../public/assets/imgpsh_fullsize-2.png"
 import sliderImg2 from "../../../public/assets/warehouse.jpg"
+import sliderImg3 from "../../../public/assets/1.jpg"
+import sliderImg4 from "../../../public/assets/2.jpg"
+import sliderImg5 from "../../../public/assets/3.jpg"
 // import whatsApp from "../../../public/assets/whatsapp123.jpg"
 import stripesBanner from "../../../public/assets/Stripes-Banner.jpg"
 import Image from 'next/image';
@@ -18,7 +21,7 @@ const Header = () => {
             <Swiper
                 spaceBetween={0}
                 slidesPerView={1}
-                navigation={true}
+                navigation={false}
                 autoplay={{
                     delay: 5000,
                     disableOnInteraction: true,
@@ -34,30 +37,66 @@ const Header = () => {
             >
                 <SwiperSlide className={styles.swiperSlide}>
                     <Link href="/">
-                        <Image src={sliderImg1} alt="Engine Banner" />
+                        <Image 
+                            src={sliderImg1}
+                            alt="Engine Banner" 
+                            width={1000} // Replace with the actual width of your image
+                            height={100} // Replace with the actual height of your image
+                            className={styles.image} 
+                        />
+                        <div className={styles.text}>👋 Welcome to SHAH PARTS</div>
                     </Link>
                 </SwiperSlide>
                 <SwiperSlide className={styles.swiperSlide}>
                     <Link href="/">
-                        <Image src={sliderImg2} alt="Engine Banner2" />
+                        <Image 
+                            src={sliderImg2} // Update the path and name accordingly
+                            alt="Engine Banner2" 
+                            width={1000} // Replace with the actual width of your image
+                            height={100} // Replace with the actual height of your image
+                            className={styles.image} 
+                        />
+                        <div className={styles.text}>SHAH PARTS Warehouse</div>
                     </Link>
                 </SwiperSlide>
-                {/* <SwiperSlide>
+                <SwiperSlide className={styles.swiperSlide}>
                     <Link href="/">
-                        <Image src={sliderImg1} alt="Engine Banner" />
+                        <Image 
+                            src={sliderImg3} // Update the path and name accordingly
+                            alt="Engine Banner3" 
+                            width={1000} // Replace with the actual width of your image
+                            height={100} // Replace with the actual height of your image
+                            className={styles.image} 
+                        />
+                        <div className={styles.text}>1. Personal/Commercial, Affordable Shipping Cost.</div>
                     </Link>
-                </SwiperSlide> */}
+                </SwiperSlide>
+                <SwiperSlide className={styles.swiperSlide}>
+                    <Link href="/">
+                        <Image 
+                            src={sliderImg4} // Update the path and name accordingly
+                            alt="Engine Banner4" 
+                            width={1000} // Replace with the actual width of your image
+                            height={100} // Replace with the actual height of your image
+                            className={styles.image} 
+                        />
+                        <div className={styles.text}>2. Port to Port Engine/Transmission Shipping.</div>
+                    </Link>
+                </SwiperSlide>
+                <SwiperSlide className={styles.swiperSlide}>
+                    <Link href="/">
+                        <Image 
+                            src={sliderImg5} // Update the path and name accordingly
+                            alt="Engine Banner5" 
+                            width={1000} // Replace with the actual width of your image
+                            height={100} // Replace with the actual height of your image
+                            className={styles.image} 
+                        />
+                        <div className={styles.text}>3. Customers Looking for Rare Japanese Engine/Transmissions, Always End Up At SHAHPARTS.</div>
+                    </Link>
+                </SwiperSlide>
+                {/* Additional slides can be added here */}
             </Swiper>
-            {/* <Col xs={24} md={8}>
-                    <Link href="https://shahparts.com/product-category/brand/toyota">
-                        <Image className='h-[110px] object-cover' src={whatsApp} alt="WhatsApp Banner" />
-                    </Link>
-                    <div className='mt-8'>
-                        <Link href="https://shahparts.com/product/african-soldier-stripes-toyota-vitz-jask3435/">
-                            <Image src={stripesBanner} alt="Stripes Banner" />
-                        </Link>
-                    </div>
-                </Col> */}
         </header>
     );
 };
